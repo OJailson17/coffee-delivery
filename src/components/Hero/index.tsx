cec5faf28eb9bade53c932b25a419aba544fcf14
OJailson17 <JaylsonO17@gmail.com>
2022-10-01 00:24:24 -0300

    styles: crete hero section

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,57 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react';
+
+import coffeeHeroImg from '../../assets/coffee-hero.png';
+import {
+	HeroContainer,
+	HeroContent,
+	HeroOptionsCart,
+	HeroOptionsClock,
+	HeroOptionsCoffee,
+	HeroOptionsPackage,
+	ImageHero,
+} from './styles';
+
+export const Hero = () => {
+	return (
+		<HeroContainer>
+			<HeroContent>
+				<h1>Encontre o café perfeito para qualquer hora do dia</h1>
+				<p>
+					Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+					hora
+				</p>
+
+				<div>
+					<HeroOptionsCart>
+						<div>
+							<ShoppingCart size={13} color='#fff' weight='fill' />
+						</div>
+						<span>Compra simples e segura</span>
+					</HeroOptionsCart>
+					<HeroOptionsPackage>
+						<div>
+							<Package size={13} color='#fff' weight='fill' />
+						</div>
+						<span>Embalagem mantém o café intacto</span>
+					</HeroOptionsPackage>
+					<HeroOptionsClock>
+						<div>
+							<Timer size={13} color='#fff' weight='fill' />
+						</div>
+						<span>Entrega rápida e rastreada</span>
+					</HeroOptionsClock>
+					<HeroOptionsCoffee>
+						<div>
+							<Coffee size={13} color='#fff' weight='fill' />
+						</div>
+						<span>O café chega fresquinho até você</span>
+					</HeroOptionsCoffee>
+				</div>
+			</HeroContent>
+
+			<ImageHero>
+				<img src={coffeeHeroImg} alt='' />
+			</ImageHero>
+		</HeroContainer>
+	);
+};
---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+
+export const HeroContainer = styled.div`
+	width: 100%;
+	max-width: 1290px;
+
+	margin: 5.875rem auto 0;
+	padding: 0 2rem;
+
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+export const HeroContent = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	h1 {
+		font-size: 3rem;
+		font-weight: 800;
+		color: ${props => props.theme.colors['base-title']};
+		font-family: ${props => props.theme.fonts.header};
+		line-height: 130%;
+		width: 36.75rem;
+	}
+
+	p {
+		font-size: 1.25rem;
+		font-weight: 400;
+		line-height: 130%;
+		width: 36.75rem;
+		color: ${props => props.theme.colors['base-subtitle']};
+
+		margin-top: 1rem;
+	}
+
+	& > div {
+		margin-top: 4.125rem;
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		row-gap: 1.5rem;
+	}
+`;
+
+export const ImageHero = styled.div`
+	img {
+		width: 29.75rem;
+		height: 22.5rem;
+	}
+`;
+
+export const HeroOptionsBase = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 0.75rem;
+
+	& > div {
+		width: 2rem;
+		height: 2rem;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		border-radius: 50%;
+	}
+
+	span {
+		font-size: 1rem;
+		color: ${props => props.theme.colors['base-text']};
+	}
+`;
+
+export const HeroOptionsCart = styled(HeroOptionsBase)`
+	& > div {
+		background-color: ${props => props.theme.colors['yellow-dark']};
+	}
+`;
+
+export const HeroOptionsPackage = styled(HeroOptionsBase)`
+	& > div {
+		background-color: ${props => props.theme.colors['base-text']};
+	}
+`;
+
+export const HeroOptionsClock = styled(HeroOptionsBase)`
+	& > div {
+		background-color: ${props => props.theme.colors.yellow};
+	}
+`;
+
+export const HeroOptionsCoffee = styled(HeroOptionsBase)`
+	& > div {
+		background-color: ${props => props.theme.colors.purple};
+	}
+`;
