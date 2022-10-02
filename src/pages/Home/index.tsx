298b5024ca55093be532e3344e0a6ce762470ba9
OJailson17 <JaylsonO17@gmail.com>
2022-10-02 02:11:40 -0300

    style: add coffee cards in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
+import { CoffeeCard } from '../../components/CoffeeCard';
 import { Header } from '../../components/Header';
 import { Hero } from '../../components/Hero';
+import { CoffeeGridContainer, MainHomeContainer } from './styles';
+
+import { coffeeData } from '../../utils/coffeeData';
 
 export const Home = () => {
 	return (
 		<>
 			<Header />
 			<Hero />
+
+			<MainHomeContainer>
+				<h2>Nossos cafés</h2>
+
+				<CoffeeGridContainer>
+					{coffeeData.map(coffee => (
+						<CoffeeCard key={coffee.title} coffee={coffee} />
+					))}
+				</CoffeeGridContainer>
+			</MainHomeContainer>
 		</>
 	);
 };
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const MainHomeContainer = styled.main`
+	width: 100%;
+	max-width: 1290px;
+
+	margin: 0 auto;
+	margin-top: 6.75rem;
+	margin-bottom: 3rem;
+
+	h2 {
+		margin-left: 1.8rem;
+		width: max-content;
+		font-family: ${props => props.theme.fonts.header};
+		font-weight: 800;
+		font-size: 2rem;
+		color: ${props => props.theme.colors['base-subtitle']};
+	}
+`;
+
+export const CoffeeGridContainer = styled.div`
+	margin-top: 3.375rem;
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	column-gap: 2rem;
+	row-gap: 2.5rem;
+	place-items: center;
+`;
