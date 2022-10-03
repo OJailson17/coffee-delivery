dbd1c793b9350c88309d9eb792d6202dec3de826
OJailson17 <JaylsonO17@gmail.com>
2022-10-02 23:01:00 -0300

    feat: add route navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-import { Home } from './pages/Home';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Router } from './Router';
+import { GlobalStyles } from './styles/global';
+import { defaultTheme } from './styles/themes/defaultTheme';
 
 export function App() {
-	return <Home />;
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={defaultTheme}>
+				<GlobalStyles />
+
+				<Router />
+			</ThemeProvider>
+		</BrowserRouter>
+	);
 }
