b7271afa3022bc93ef436a8a14d4ef60aa324492
OJailson17 <JaylsonO17@gmail.com>
2022-10-08 03:48:32 -0300

    feat: show delivery info

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,4 +1,5 @@
 import { CurrencyDollar, MapPin, Timer } from 'phosphor-react';
+import { useLocation } from 'react-router-dom';
 import motoboyImg from '../../assets/motoboy.svg';
 import {
 	InfoLocation,
@@ -10,7 +11,28 @@ import {
 	SuccessContent,
 } from './styles';
 
+interface IStateReturn {
+	state: {
+		city: string;
+		houseNumber: number;
+		neighbor: string;
+		street: string;
+		uf: string;
+		paymentMethod: 'credit-card' | 'debit-card' | 'cash';
+	};
+}
+
 export const Success = () => {
+	const { state } = useLocation() as IStateReturn;
+
+	console.log({ state });
+
+	const paymentMethod = {
+		'credit-card': 'Cartão de Crédito',
+		'debit-card': 'Cartão de Débito',
+		cash: 'Dinheiro',
+	};
+
 	return (
 		<SuccessContainer>
 			<h1>Uhu! Pedido confirmado</h1>
@@ -26,9 +48,15 @@ export const Success = () => {
 
 							<div>
 								<p>
-									Entrega em <strong> Rua João Daniel Martinelli, 102</strong>
+									Entrega em{' '}
+									<strong>
+										{' '}
+										Rua {state.street}, {state.houseNumber}
+									</strong>
 								</p>
-								<p>Farrapos - Porto Alegre, RS</p>
+								<p>
+									{state.neighbor} - {state.city}, {state.uf}
+								</p>
 							</div>
 						</InfoLocation>
 
@@ -52,7 +80,7 @@ export const Success = () => {
 
 							<div>
 								<p>Pagamento na entrega</p>
-								<strong>Cartão de Crédito</strong>
+								<strong>{paymentMethod[state.paymentMethod]}</strong>
 							</div>
 						</InfoPaymentMethod>
 					</OrderInfo>
